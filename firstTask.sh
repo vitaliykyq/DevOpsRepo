033877092adbd05a01d021a68c95bb4f0efec39a
Vitaliy Kybych <v.kyb@ukr.net>
2022-07-16 15:09:10 +0300

    Edit firstTask.sh

--- firstTask.sh
+++ firstTask.sh
@@ -4,3 +4,5 @@ mkdir -p KYB/{KPL/GR1,KTT,KNN/GR2}
 
 echo -e "- 2022\n- 15.07.2022\n- vitaliykyq@gmail.com" > KYB/KPL/GR1/Vitaliy.txt
 echo -e "- 2022\n- 15.07.2022\n- vitaliykyq@gmail.com" > KYB/KNN/GR2/Kybych.txt 
+
+echo "TEST TEXT"
